a50ef553cfb6133e19f36d58c73d912e28534ac4
oregami <oregami@163.com>
2016-12-17 00:54:56 +0800

    Initial Commit

---
+++ UIKitCocos2dExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIKitCocos2dExample
+//
+//  Created by oregami on 2016/12/17.
+//  Copyright © 2016年 oregami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIKitCocos2dExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UIKitCocos2dExample
+//
+//  Created by oregami on 2016/12/17.
+//  Copyright © 2016年 oregami. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
